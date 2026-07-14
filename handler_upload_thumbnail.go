d3122482ed8ee18d076e77229d1f6c57a458b1c2
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2026-07-14 15:53:00 +0200

    removed bad plain/binary text as saved data to file system save location

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -33,7 +35,12 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	const maxMemory = 10 << 20
-	r.ParseMultipartForm(maxMemory)
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "could not parse memory block", err)
+		return
+	}
+
 	file, header, err := r.FormFile("thumbnail")
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
@@ -42,12 +49,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	defer file.Close()
 
 	mediaType := header.Header.Get("Content-Type")
-
-	imageData, err := io.ReadAll(file)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "failed to read file data", err)
-		return
-	}
 
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
@@ -60,14 +61,34 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	str := base64.StdEncoding.EncodeToString(imageData)
-	dataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, str)
+	parts := strings.Split(mediaType, "/")
+	if len(parts) < 2 {
+		respondWithError(w, http.StatusBadRequest, "could not split mediaType for extention extraction", nil)
+		return
+	}
+	extension := parts[1]
+	filename := fmt.Sprintf("%s.%s", videoID, extension)
+	dataURL := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, filename)
+	fullPath := filepath.Join(cfg.assetsRoot, filename)
+
+	dst, err := os.Create(fullPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "could create full file path", err)
+		return
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "could not copy file to destination", err)
+		return
+	}
 
 	video.ThumbnailURL = &dataURL
 
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "could not update vidoe", err)
+		respondWithError(w, http.StatusInternalServerError, "could not update video", err)
 		return
 	}
 
